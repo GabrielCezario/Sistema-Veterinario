ee7623b1dde0cacd3b947b43146fee9e9487eeb2
Guilherme Silveira <Guilherme Silveira@LAPTOP-O2FF83PG>
2018-11-23 20:21:06 -0200

    IMPLEMENTAÇÃO CONTROLADOR CADASTRO VETERINÁRIO

--- SistemaVeterinario/src/Controller/ControladorCadastroVeterinario.java
+++ SistemaVeterinario/src/Controller/ControladorCadastroVeterinario.java
@@ -3,6 +3,124 @@ package Controller;
 import Model.Veterinario;
 
 public class ControladorCadastroVeterinario {
-
-
+	
+	
+// >================================ MÉTODOS PRIVADOS ======================================<
+	
+	private VeterinarioInvalidoException validarVeterinario(Veterinario veterinario) {
+		
+		VeterinarioInvalidoException veterinarioInvalidoException = null;
+		
+		if (veterinario.getNome() == null || veterinario.getNome().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setNomeVazio(true);
+			
+		}
+		
+		if (veterinario.getDtNascimento() == null || veterinario.getDtNascimento().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setDtNascimentoVazio(true);
+			
+		}
+		
+		if (veterinario.getTelefone() == null || veterinario.getTelefone().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setTelefoneVazio(true);
+			
+		}
+		
+		if (veterinario.getEndereco() == null || veterinario.getEndereco().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setEnderecoVazio(true);
+			
+		}
+		
+		if (veterinario.getSexo() == null || veterinario.getSexo().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setSexoVazio(true);
+			
+		}
+		
+		if (veterinario.getEmail() == null || veterinario.getEmail().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setEmailVazio(true);
+			
+		}
+		
+		if (veterinario.getLogin() == null || veterinario.getLogin().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setLoginVazio(true);
+			
+		}
+		
+		if (veterinario.getSenha() == null || veterinario.getSenha().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setSenhaVazio(true);
+			
+		}
+		
+		if (veterinario.getEspecialidade() == null || veterinario.getEspecialidade().trim().isEmpty()) {
+			
+			if (veterinarioInvalidoException == null) {
+				
+				veterinarioInvalidoException = new VeterinarioInvalidoException();
+				
+			}
+			
+			veterinarioInvalidoException.setEspecialidadeVazio(true);
+			
+		}
+		
+		return veterinarioInvalidoException;
+		
+	}
+	
 }
